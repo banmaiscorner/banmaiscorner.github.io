e646d153c203e1e8125c8e96d21a5d846b52be17
banmai0610 <banmai.artist@gmail.com>
2017-02-14 00:30:40 -0700

    update time calculator function

--- time_changing.js
+++ time_changing.js
@@ -12,5 +12,5 @@ var n = current_time.getTimezoneOffset();
 //my timezone
 var my_time = new Date(current_time.getTime() + n*60000 - 420*60000);
 
-document.write("Your current time is: "+current_time.getMonth()+"/"+current_time.getDate()+"/"+current_time.getYear()+" "+current_time.getHours()+":"+current_time.getMinutes()+"</br>");
-document.write("My current time is: "+my_time.getMonth()+"/"+my_time.getDate()+"/"+my_time.getYear()+" "+my_time.getHours()+":"+my_time.getMinutes());
+document.write("Your current time is: "+current_time.getMonth()+"/"+current_time.getDate()+"/"+current_time.getFullYear()+" "+current_time.getHours()+":"+current_time.getMinutes()+"</br>");
+document.write("My current time is: "+my_time.getMonth()+"/"+my_time.getDate()+"/"+my_time.getFullYear()+" "+my_time.getHours()+":"+my_time.getMinutes());
