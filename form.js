85294db968fbb5b4a151932279cb7a3b5a2694bc
banmai0610 <banmai.artist@gmail.com>
2017-03-20 21:58:37 -0700

    contact _temp

--- form.js
+++ form.js
@@ -3,15 +3,19 @@ function submit() {
 	var name = document.getElementById("inputname");
 	var email = document.getElementById("inputemail");
 	var message = document.getElementById("inputmessage");
+	//name alert
 	if (name.value === "") {
 		alert("Please fill in your name.");
 	}
+	//email alert
 	if (email.value === "") {
 		alert("Please fill in your email.");
 	}
+	//message alert
 	if (message.value === "") {
 		alert("Please fill in your message.");
 	}
+	//fully filled
 	if (name.value && message.value && email.value) {
 		alert("Thank you !");
 	}
