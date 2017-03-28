396bcc1fc0d3b41292fe52b52197d62ca0833026
banmai0610 <banmai.artist@gmail.com>
2017-03-27 22:26:19 -0700

    date for tem

--- form.js
+++ form.js
@@ -3,21 +3,40 @@ function submit() {
 	var name = document.getElementById("inputname");
 	var email = document.getElementById("inputemail");
 	var message = document.getElementById("inputmessage");
+	var phone = document.getElementById("inputphone");
+	var date = document.getElementById("inputdate").value;
+	
 	//name alert
 	if (name.value === "") {
-		alert("Please fill in your name.");
+		document.getElementById("name_alert").innerHTML="Please fill in your name.";
 	}
 	//email alert
 	if (email.value === "") {
-		alert("Please fill in your email.");
+		document.getElementById("email_alert").innerHTML="Please fill in your email.";
 	}
 	//message alert
 	if (message.value === "") {
-		alert("Please fill in your message.");
+		document.getElementById("message_alert").innerHTML="Please fill in your message.";
 	}
-	//fully filled
-	if (name.value && message.value && email.value) {
-		alert("Thank you !");
+	
+	//phone alert
+	if (phone.value === "") {
+		document.getElementById("phone_alert").innerHTML="Please fill in your phone.";
 	}
+	
+	//phone alert
+	if (date === "") {
+		document.getElementById("date_alert").innerHTML="Please pick your date.";
+	} else {
+		var cal = new Date(date);
+		var m_Month =  cal.getMonth();
+		var m_date = cal.getDate();
+		var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+		m_Month = month[m_Month];
+		document.getElementById("date_alert").innerHTML="You picked "+m_Month+" "+m_date+", "+cal.getFullYear()+" as the due date";
+		
+		
+	}
+	
 	document.getElementById("submit_button").addEventListener("click", submit, false);
 }
