f9d7869ab425a128fe9dd8f5917f63f36788b3b7
banmai0610 <banmai.artist@gmail.com>
2017-04-17 21:07:48 -0700

    map2

--- Maps.js
+++ Maps.js
@@ -1,10 +1,28 @@
 // JavaScript Document
-
-function myMap() {
+var waitForUser;
+function geoTest() {
 	"use strict";
-	var mapProp= {
-		center:new google.maps.LatLng(51.508742,-0.120850),
-		zoom:5,
+	waitForUser = setTimeout(fail, 10000);
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(createMap, fail, {timeout: 10000});
+	} else {
+		fail();
+	}
+}
+
+function createMap(position) {
+	"use strict";
+	clearTimeout(waitForUser);
+	var Lat = position.coords.latitude;
+	var Lng = position.coords.longitude;
+	var mapOptions = {
+		center: new google.maps.LatLng(Lat, Lng), 
+		zoom: 10
 	};
-	var map=new google.maps.Map(document.getElementById("GoogleMap"),mapProp);
+	var map = new google.maps.Map(document.getElementById("GoogleMap"), mapOptions);
+}
+
+function fail() {
+	"use strict";
+	document.getElementById("GoogleMap").innerHTML = "Unable to access your current location";
 }
